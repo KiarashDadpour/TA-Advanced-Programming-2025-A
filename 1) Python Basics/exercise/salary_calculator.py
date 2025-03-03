f28bfe9652e81f1f68f11f0f3e8a5d6cc1d2bb01
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-03-04 00:43:57 +0330

    Create salary_calculator.py

---
+++ 1) Python Basics/exercise/salary_calculator.py
@@ -0,0 +1,11 @@
+''' 
+Inputs: 
+      Enter employee name: Sarah  
+      Enter hourly wage: 15  
+      Enter hours worked: 40  
+
+Outputs: 
+      Gross Salary: 600.00$  
+      Tax Deduction (10%): 60.00$  
+      Net Salary: 540.00$  
+'''
