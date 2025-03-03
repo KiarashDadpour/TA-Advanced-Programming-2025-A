1996ab34c4c29330fd830689ca9ed061ca966234
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-03-04 00:56:26 +0330

    Create bitcoin.py

---
+++ 1) Python Basics/exercise/bitcoin.py
@@ -0,0 +1,9 @@
+"""
+Inputs: 
+      Enter the number of Bitcoins you have: 2
+      Enter the current price of 1 Bitcoin: 45000
+
+Outputs: 
+      The total value of your Bitcoin is: 90000.00 USD
+      
+'''
