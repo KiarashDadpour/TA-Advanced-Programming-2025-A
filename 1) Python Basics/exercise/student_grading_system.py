bf21787f7fda264b1ef10497d5a8652ea638be09
Kiarash Dadpour <140727992+KiarashDadpour@users.noreply.github.com>
2025-03-04 00:47:00 +0330

    Create student_grading_system.py

---
+++ 1) Python Basics/exercise/student_grading_system.py
@@ -0,0 +1,11 @@
+'''
+Inputs: 
+      Enter student name: John  
+      Enter grade for Math: 85  
+      Enter grade for Science: 92  
+      Enter grade for History: 78  
+
+Outputs: 
+      Student: John  
+      Average GPA: 85.0
+'''
